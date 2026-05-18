0ff375679ec5edb5ff89ea6541b1d71891aa1dd9
tunav602-pixel <tunav602@gmail.com>
2026-05-18 16:34:38 +0700

    Add files via upload

---
+++ B1SS1.py
@@ -0,0 +1,9 @@
+print("-------- HỆ THỐNG TIẾP NHẬN BỆNH NHÂN --------")
+name_patient = input("Nhập tên bệnh nhân: ")
+age = int(input("Mời bạn nhập tuổi: "))
+symptom = input("Mời bạn nhập triệu chứng bệnh ")
+
+print("--- PHIẾU KHÁM BỆNH ---")
+print("Tên bệnh nhân: ", name_patient)
+print("Tuổi", age)
+print("Triệu chứng", symptom)
