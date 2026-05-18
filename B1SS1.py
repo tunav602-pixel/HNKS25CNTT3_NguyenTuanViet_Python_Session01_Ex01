2b499e9567f0b52fa35ec4ad303bef60b46d223b
tunav602-pixel <tunav602@gmail.com>
2026-05-18 21:58:05 +0700

    Add files via upload

--- B1SS1.py
+++ B1SS1.py
@@ -4,6 +4,6 @@ age = int(input("Mời bạn nhập tuổi: "))
 symptom = input("Mời bạn nhập triệu chứng bệnh ")
 
 print("--- PHIẾU KHÁM BỆNH ---")
-print("Tên bệnh nhân: ", name_patient)
-print("Tuổi", age)
-print("Triệu chứng", symptom)
+print("Tên bệnh nhân: ", name_patient) #Tên bệnh nhân phải là name_patien chứ không phải là symptom (symptom là tên triệu chứng)
+print("Tuổi", age) #Tuổi trong chương trình đã cho sẵn đang bị sai kiểu dữ liệu / tuổi người dùng nhập vào được lưu vào biến age nhưng chương tình đã cho lại xuất dữ liệu chuỗi là name_patient
+print("Triệu chứng", symptom) # Tương tự như tên và tuổi thì chương trình đã cho đang xuất ra sai kiểu dũ liệu của triệu chứng (symptom)
